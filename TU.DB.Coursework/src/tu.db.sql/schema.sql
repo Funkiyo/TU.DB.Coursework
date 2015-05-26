ab3c57f07891e357defaa997dd637e607c098430
Donald Ovcharov <donaldovcharov@gmail.com>
2015-05-27 02:34:49 +0300

    schema update

--- TU.DB.Coursework/src/tu.db.sql/schema.sql
+++ TU.DB.Coursework/src/tu.db.sql/schema.sql
@@ -40,3 +40,11 @@ CREATE TABLE `register` (
     FOREIGN KEY (`car_num`)
         REFERENCES `cars`(`reg_num`)
 ) ENGINE = InnoDB;
+
+INSERT INTO `parkings`.`car_companies` (`id`, `name`) VALUES (NULL, 'BMW'), (NULL, 'Ford');
+INSERT INTO `parkings`.`car_companies` (`id`, `name`) VALUES (NULL, 'Opel'), (NULL, 'VW');
+
+INSERT INTO `parkings`.`car_models` (`id`, `name`, `company_id`) VALUES (NULL, 'E36', '1'), (NULL, 'E39', '1');
+INSERT INTO `parkings`.`car_models` (`id`, `name`, `company_id`) VALUES (NULL, 'Focus', '2'), (NULL, 'Mustang', '2');
+INSERT INTO `parkings`.`car_models` (`id`, `name`, `company_id`) VALUES (NULL, 'Astra', '3'), (NULL, 'Corsa', '3');
+INSERT INTO `parkings`.`car_models` (`id`, `name`, `company_id`) VALUES (NULL, 'Golf', '4'), (NULL, 'Polo', '4');
